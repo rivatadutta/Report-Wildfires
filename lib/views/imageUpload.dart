dcc997df4c09a6160bcec299184b6d37483ac144
Atticus Jones <atticusjonesslc@gmail.com>
2020-10-20 18:35:22 -0600

    added exif_flutter plugin

--- lib/views/imageUpload.dart
+++ lib/views/imageUpload.dart
@@ -1,6 +1,19 @@
+import 'dart:async';
 import 'dart:io';
-import 'package:image_picker/image_picker.dart';
+import 'dart:typed_data';
+
+import 'package:exif_flutter/exif_flutter.dart';
+import 'package:exif_flutter/tags.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image_picker/image_picker.dart';
+// import 'package:gallery_saver/gallery_saver.dart';
+
+Future<void> writeToFile(ByteData data, String path) {
+  final buffer = data.buffer;
+  return new File(path)
+      .writeAsBytes(buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
+}
 
 class ImageUpload extends StatefulWidget {
   ImageUpload({Key key, this.title}) : super(key: key);
@@ -12,8 +25,50 @@ class ImageUpload extends StatefulWidget {
 }
 
 class _ImageUploadState extends State<ImageUpload> {
+  File result;
   File _image;
   final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  getMetadata() async {
+    File file = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (file != null) {
+      final fileBytes = await file.readAsBytes();
+      mainCheckFlow(ByteData.view(fileBytes.buffer));
+    }
+  }
+
+  Future<void> mainCheckFlow(ByteData bytes) async {
+    final tempPath = await getTemporaryDirectory();
+    final filePath = tempPath.path + '/' + 'New Image - ${DateTime.now()}.jpeg';
+    await writeToFile(
+      bytes,
+      filePath,
+    );
+
+    final attributesFirst = await Exif.getAttributes(filePath);
+    print(attributesFirst);
+
+    // final latitude = -4.8055555;
+    // final longitude = -39.3555555;
+    // final dateTimeOriginal = DateTime.parse('2009-08-11 16:45:32');
+    // final userComment = 'You can add a metadata stringified version here';
+    //
+    // final newAttributes = Metadata(
+    //   latitude: latitude,
+    //   longitude: longitude,
+    //   dateTimeOriginal: dateTimeOriginal,
+    //   userComment: userComment,
+    // );
+    //
+    // await Exif.setAttributes(filePath, newAttributes);
+    // final attributesSecond = await Exif.getAttributes(filePath);
+    // print(attributesSecond);
+  }
 
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
@@ -39,7 +94,7 @@ class _ImageUploadState extends State<ImageUpload> {
             : Image.file(_image),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
+        onPressed: getMetadata,
         tooltip: 'Pick Image',
         child: Icon(Icons.add_a_photo),
       ), // This trailing comma makes auto-formatting nicer for build methods.
