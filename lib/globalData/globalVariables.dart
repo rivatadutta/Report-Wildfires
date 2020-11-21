eb2db98ef28fba355cb2e891207de8ed1f2a92f4
Ulysses Uribe <ulysses.uribe@gmail.com>
2020-11-20 18:08:52 -0800

    added globals

--- lib/globalData/globalVariables.dart
+++ lib/globalData/globalVariables.dart
@@ -1,8 +1,13 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class Global {
   static var backgroundColor = 0xffFFD9B3;
   static var iconColor = 0xffFF824D;
   static var selectedIconColor = 0xffCC6A40;
   static var displayName;
   static var userAddress;
+  static List<LatLng> resultCords = [];
+  static var lat;
+  static var long;
   static const kGoogleApiKey = "AIzaSyBIzb_dLgIxzWKqieUe0-5Ak-gTBhklNJI";
 }
